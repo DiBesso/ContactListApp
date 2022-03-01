844eb37e330dcb7eb792dddd5dd1c7ccad2a37df
Dmitry <radiator211@mail.ru>
2022-03-01 14:05:05 +0700

    Initial Commit

---
+++ ContactListApp/ContactListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListAppApp.swift
+//  ContactListApp
+//
+//  Created by Дмитрий Бессонов on 01.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContactListApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ContactListApp
+//
+//  Created by Дмитрий Бессонов on 01.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
